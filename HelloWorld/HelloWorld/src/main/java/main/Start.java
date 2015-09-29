f19a30aec1cf5dd51a8d44f9f78f07819f913f55
Colin Glynn Davidson <colin_g_davidson@msn.com>
2015-09-29 01:19:07 +0100

    .

---
+++ HelloWorld/HelloWorld/src/main/java/main/Start.java
@@ -0,0 +1,20 @@
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Random;
+
+public class Start {
+
+/***************************************************
+This program is a General Algortihm for solving a 
+simple solution to finding a good circuit for a 
+bike tour.  We will base the best result as one that
+visits every city and takes the shortest distance.  
+
+**************************************************/
+
+	public static void main(String[] s) {
+		System.out.println("Hello, World!");
+	}
+}
