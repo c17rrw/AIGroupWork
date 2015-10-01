3da99e3182a54dca28856ed4574c50e9cede96fb
Léon McGregor <lonmcgregor@outlook.com>
2015-10-01 20:50:04 +0100

    U: Updated GA Skeleton
    Added fuller method bodies to the GA Skeleton

--- HelloWorld/HelloWorld/src/main/java/main/RouteSearch.java
+++ HelloWorld/HelloWorld/src/main/java/main/RouteSearch.java
@@ -1,31 +1,54 @@
 import java.util.Random;
-import java.util.ArrayList;
+
+//TODO: Fix tabs
 
 /*
 * Class for Route Search Genetic Algorithm
 */
 public class RouteSearch{
 	
-	ArrayList<Object> currentPopulation;
+	private final int MAX_POPULATION_SIZE = 4;
+	
+	//TODO: Decide whether to pass arguments or store them as private fields
+	Object[] currentPopulation;
+	Object[] newSolution;
+	int[] newPopulationScores;
 	
 	public RouteSearch(){
-		currentPopulation = new ArrayList<Object>();
 		System.out.println("Have created a route search algorithm");
+		generateInitialPopulation();
 	}
 	
-	public void generateInitialPopulation(){}
+	private void generateInitialPopulation(){
+		currentPopulation = new Object[MAX_POPULATION_SIZE];
+	}
 	
 	public void iterateOneStep(){
-		ArrayList<Object> newPopulation = currentPopulation;
-		calculateFitnessOfAll();
-		keepFittestSolutions();
+		Object[] newPopulation = currentPopulation;
+		calculateFitnessOfAll(newPopulation);
+		keepFittestSolutions(newPopulation, newPopulationScores);
 		generateNewSolutions();
 	}
 	
-	public void calculateFitnessOfAll(){}
-	public int calculateFitnessOfOne(Object solution){}
-	public void keepFittestSolutions(){}
-	public void generateNewSolutions(){}
-	public Object makeChild(Object mother, Object father){}
-	public Object mutateChild(Object child){}
+	private void calculateFitnessOfAll(Object[] newPopulation){
+		newPopulationScores = new int[MAX_POPULATION_SIZE];
+		for(int i = 0; i < newPopulation.length; i++){
+			newPopulationScores[i] = calculateFitnessOfOne(newPopulation[i]);
+		}
+	}
+	private int calculateFitnessOfOne(Object solution){
+		return 0;
+	}
+	private void keepFittestSolutions(Object[] newPopulation, int[] newPopulationScores){
+	
+	}
+	private void generateNewSolutions(){
+	
+	}
+	private Object makeChild(Object mother, Object father){
+		return new Object();
+	}
+	private Object mutateChild(Object child){
+		return new Object();
+	}
 }
