107a052d8ffc5dd0679f0e233cad23b27b379119
Léon McGregor <lonmcgregor@outlook.com>
2015-10-05 12:24:23 +0100

    U: Added TODO comments
    Tried to denote more easily what needs to be done with inline TODO
    comments

--- HelloWorld/HelloWorld/src/main/java/main/RouteSearch.java
+++ HelloWorld/HelloWorld/src/main/java/main/RouteSearch.java
@@ -1,28 +1,39 @@
 import java.util.Random;
 
-//TODO: Fix tabs
+/**
+TODO.
+	How to encode solutions [Genotype]
+	What level of randomness to have for mutating
+	How large should population be
+	How to splice parent genes
+	How many of the fittest to keep
+**/
 
-/*
-* Class for Route Search Genetic Algorithm
-*/
+/**
+* Class for Route Search Genetic Algorithm.
+**/
 public class RouteSearch{
 	
 	private final int MAX_POPULATION_SIZE = 4;
 	
-	//TODO: Decide whether to pass arguments or store them as private fields
+	private Random random;
+	
 	Object[] currentPopulation;
 	Object[] newSolution;
 	int[] newPopulationScores;
 	
 	public RouteSearch(){
 		System.out.println("Have created a route search algorithm");
+		random = new Random();
 		generateInitialPopulation();
 	}
 	
+	/**TODO**/
 	private void generateInitialPopulation(){
 		currentPopulation = new Object[MAX_POPULATION_SIZE];
 	}
 	
+	/**TODO**/
 	public void iterateOneStep(){
 		Object[] newPopulation = currentPopulation;
 		calculateFitnessOfAll(newPopulation);
@@ -30,25 +41,36 @@ public class RouteSearch{
 		generateNewSolutions();
 	}
 	
+	/**TODO**/
 	private void calculateFitnessOfAll(Object[] newPopulation){
 		newPopulationScores = new int[MAX_POPULATION_SIZE];
 		for(int i = 0; i < newPopulation.length; i++){
 			newPopulationScores[i] = calculateFitnessOfOne(newPopulation[i]);
 		}
 	}
+	
+	/**TODO**/
 	private int calculateFitnessOfOne(Object solution){
 		return 0;
 	}
+	
+	/**TODO**/
 	private void keepFittestSolutions(Object[] newPopulation, int[] newPopulationScores){
 	
 	}
+	
+	/**TODO**/
 	private void generateNewSolutions(){
 	
 	}
+	
+	/**TODO**/
 	private Object makeChild(Object mother, Object father){
 		return new Object();
 	}
+	
+	/**TODO**/
 	private Object mutateChild(Object child){
-		return new Object();
+		return random.nextBoolean() ? child : new Object();
 	}
 }
--- HelloWorld/HelloWorld/src/main/java/main/Start.java
+++ HelloWorld/HelloWorld/src/main/java/main/Start.java
@@ -1,8 +1,5 @@
-
-
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.Random;
 
 public class Start {
 
@@ -14,8 +11,21 @@ visits every city and takes the shortest distance.
 
 **************************************************/
 
+	private static final int MAX_ITERATIONS = 50;
+
+	/**TODO.
+	**		What to print after each iteration is completed
+	**		Find a way to pass in the distances matrix
+	**			[probably only used in fitness calculation]
+	**		For part 4 is it worth allowing automation for 
+	**			setting GA params from within here?
+	**/
 	public static void main(String[] s) {
 		System.out.println("Hello, World!");
 		RouteSearch routeSearch = new RouteSearch();
+		for(int i = 0; i < MAX_ITERATIONS; i++){
+			routeSearch.iterateOneStep();
+			System.out.println("Iteration "+i);
+		}
 	}
 }
